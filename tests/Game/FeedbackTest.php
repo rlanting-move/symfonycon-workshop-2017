d4cebe4f1f83cc6304d68c53c5fd5bfc941ec77b
Jakub Zalas <jakub@zalas.pl>
2017-11-13 08:56:12 +0000

    [2] (Feedback) Define a test case - it exposes the guess code, exact and colour hits

--- tests/Game/FeedbackTest.php
+++ tests/Game/FeedbackTest.php
@@ -7,8 +7,16 @@ use PHPUnit\Framework\TestCase;
 
 class FeedbackTest extends TestCase
 {
-    public function test_it_is_initialized()
+    public function test_it_exposes_guess_code_and_exact_and_colour_hits()
     {
-        $this->assertInstanceOf(Feedback::class, new Feedback());
+        $guessCode = $this->prophesize(Code::class)->reveal();
+        $exactHits = 2;
+        $colourOnlyHits = 1;
+
+        $feedback = new Feedback($guessCode, $exactHits, $colourOnlyHits);
+
+        $this->assertSame($guessCode, $feedback->guessCode());
+        $this->assertSame($exactHits, $feedback->exactHits());
+        $this->assertSame($colourOnlyHits, $feedback->colourHits());
     }
 }
