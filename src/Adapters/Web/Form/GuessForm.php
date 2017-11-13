180b45e52e09ab10f0e5e5d88c0c0367fdcb19ca
Jakub Zalas <jakub@zalas.pl>
2017-11-13 14:23:27 +0000

    [5] Define number_of_pegs as a form option
    There was 1 error:
    
    1) SymfonyCon\Mastermind\Adapters\Web\Form\GuessFormTest::test_submit_creates_code
    Symfony\Component\OptionsResolver\Exception\UndefinedOptionsException: The option "number_of_pegs" does not exist. Defined options are: "action
    ", "attr", "auto_initialize", "block_name", "by_reference", "compound", "data", "data_class", "disabled", "empty_data", "error_bubbling", "inhe
    rit_data", "label", "label_attr", "label_format", "mapped", "method", "post_max_size_message", "property_path", "required", "translation_domain
    ", "trim", "upload_max_size_message".

--- src/Adapters/Web/Form/GuessForm.php
+++ src/Adapters/Web/Form/GuessForm.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 namespace SymfonyCon\Mastermind\Adapters\Web\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class GuessForm extends AbstractType
 {
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setRequired(['number_of_pegs']);
+    }
 }
