381df3106a18f8300a4a17b4630f5a15a5168d7b
Jakub Zalas <jakub@zalas.pl>
2017-11-13 11:14:46 +0000

    [4] (RandomCodeMaker) Make the test pass

---
+++ src/Game/RandomCodeMaker.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace SymfonyCon\Mastermind\Game;
+
+final class RandomCodeMaker implements CodeMaker
+{
+    public function newCode(int $length): Code
+    {
+    }
+}
