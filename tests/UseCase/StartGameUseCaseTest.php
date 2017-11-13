a4480e7893580b2ae2799db15eab09970d0fcae6
Jakub Zalas <jakub@zalas.pl>
2017-11-13 10:01:49 +0000

    [3] (StartGameUseCase) Define a test case - execute() stores a new decoding board

--- tests/UseCase/StartGameUseCaseTest.php
+++ tests/UseCase/StartGameUseCaseTest.php
@@ -4,11 +4,55 @@ declare(strict_types=1);
 namespace SymfonyCon\Mastermind\UseCase;
 
 use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Prophecy\Prophecy\ObjectProphecy;
+use SymfonyCon\Mastermind\Game\Code;
+use SymfonyCon\Mastermind\Game\CodeMaker;
+use SymfonyCon\Mastermind\Game\DecodingBoard;
+use SymfonyCon\Mastermind\Game\DecodingBoards;
 
 class StartGameUseCaseTest extends TestCase
 {
-    public function test_it_is_initialized()
+    const NUMBER_OF_ATTEMPTS = 12;
+    const CODE_LENGTH = 4;
+
+    /**
+     * @var StartGameUseCase
+     */
+    private $useCase;
+
+    /**
+     * @var DecodingBoards|ObjectProphecy
+     */
+    private $boards;
+
+    /**
+     * @var CodeMaker|ObjectProphecy
+     */
+    private $codeMaker;
+
+    protected function setUp()
     {
-        $this->assertInstanceOf(StartGameUseCase::class, new StartGameUseCase());
+        $this->boards = $this->prophesize(DecodingBoards::class);
+        $this->codeMaker = $this->prophesize(CodeMaker::class);
+        $this->useCase = new StartGameUseCase(
+            $this->boards->reveal(),
+            $this->codeMaker->reveal(),
+            self::NUMBER_OF_ATTEMPTS
+        );
+    }
+
+    public function test_it_stores_a_new_decoding_board()
+    {
+        $this->codeMaker->newCode(self::CODE_LENGTH)->willReturn($this->getCode());
+
+        $this->useCase->execute(self::CODE_LENGTH);
+
+        $this->boards->put(Argument::type(DecodingBoard::class))->shouldHaveBeenCalled();
+    }
+
+    private function getCode(): Code
+    {
+        return $this->prophesize(Code::class)->reveal();
     }
 }
