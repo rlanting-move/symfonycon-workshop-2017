c5410adf017e2c7fb106b8c77031bcfd9c4fc606
Jakub Zalas <jakub@zalas.pl>
2017-11-13 09:37:26 +0000

    [3] (DecodingBoard) Make the test pass

--- src/Game/DecodingBoard.php
+++ src/Game/DecodingBoard.php
@@ -5,6 +5,37 @@ namespace SymfonyCon\Mastermind\Game;
 
 class DecodingBoard
 {
+    /**
+     * @var GameUuid
+     */
+    private $gameUuid;
+
+    /**
+     * @var Code
+     */
+    private $secretCode;
+
+    /**
+     * @var int
+     */
+    private $numberOfAttempts;
+
+    public function __construct(GameUuid $gameUuid, Code $secretCode, int $numberOfAttempts)
+    {
+        $this->gameUuid = $gameUuid;
+        $this->secretCode = $secretCode;
+        $this->numberOfAttempts = $numberOfAttempts;
+    }
+
+    public function makeGuess(Code $code)
+    {
+        return new Feedback(
+            $code,
+            $this->secretCode->exactHits($code),
+            $this->secretCode->colourHits($code)
+        );
+    }
+
     public function lastFeedback()
     {
         return new Feedback(Code::fromString('Red'), 0, 0);
--- src/UseCase/ViewDecodingBoardUseCase.php
+++ src/UseCase/ViewDecodingBoardUseCase.php
@@ -3,12 +3,14 @@ declare(strict_types=1);
 
 namespace SymfonyCon\Mastermind\UseCase;
 
+use SymfonyCon\Mastermind\Game\Code;
 use SymfonyCon\Mastermind\Game\DecodingBoard;
+use SymfonyCon\Mastermind\Game\GameUuid;
 
 class ViewDecodingBoardUseCase
 {
     public function execute($gameUuid)
     {
-        return new DecodingBoard();
+        return new DecodingBoard(GameUuid::generated(), Code::fromString('Red'), 12);
     }
 }
