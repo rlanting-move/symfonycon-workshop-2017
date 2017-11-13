3fe9a958d53a8f36c24c8df7b7575effab87cd2e
Jakub Zalas <jakub@zalas.pl>
2017-11-13 08:39:42 +0000

    [2] (Code) Make the test pass
    fix the following failure:
    
    There was 1 failure:
    
    1) SymfonyCon\Mastermind\Game\CodeTest::test_it_is_created_from_a_string_of_code_pegs
    Failed asserting that two arrays are equal.
    --- Expected
    +++ Actual
    @@ @@
     Array (
    -    0 => SymfonyCon\Mastermind\Game\CodePeg Object (...)
    -    1 => SymfonyCon\Mastermind\Game\CodePeg Object (...)
    -    2 => SymfonyCon\Mastermind\Game\CodePeg Object (...)
    
    /symfonycon/tests/Game/CodeTest.php:21

--- src/Game/Code.php
+++ src/Game/Code.php
@@ -5,13 +5,33 @@ namespace SymfonyCon\Mastermind\Game;
 
 class Code
 {
+    /**
+     * @var CodePeg[]
+     */
+    private $codePegs;
+
+    /**
+     * @param CodePeg[] $codePegs
+     */
+    private function __construct(array $codePegs)
+    {
+        $this->codePegs = $codePegs;
+    }
+
     public static function fromString($codeString)
     {
-        return new self();
+        return new self(
+            array_map(
+                function (string $colour) {
+                    return new CodePeg($colour);
+                },
+                explode(' ', $codeString)
+            )
+        );
     }
 
     public function pegs()
     {
-        return [];
+        return $this->codePegs;
     }
 }
