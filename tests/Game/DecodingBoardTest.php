e8bbb7ed8216ee62685610ff290b7689a33ca38b
Jakub Zalas <jakub@zalas.pl>
2017-11-13 09:59:16 +0000

    [3] (DecodingBoard) Define a test case - the game is finished if there is no more attempts left

--- tests/Game/DecodingBoardTest.php
+++ tests/Game/DecodingBoardTest.php
@@ -176,4 +176,15 @@ class DecodingBoardTest extends TestCase
 
         $this->assertFalse($this->board->isGameFinished());
     }
+
+    public function test_the_game_is_finished_if_there_is_no_more_attempts_left()
+    {
+        $this->secretCode->exactHits($this->guessCode)->willReturn(1);
+
+        for ($i = 0; $i < self::NUMBER_OF_ATTEMPTS; $i++) {
+            $this->board->makeGuess($this->guessCode->reveal());
+        }
+
+        $this->assertTrue($this->board->isGameFinished());
+    }
 }
