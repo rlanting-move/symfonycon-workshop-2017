39a5971d81d290af2cc23749cbe7c6fa144f30f7
Jakub Zalas <jakub@zalas.pl>
2017-11-13 10:18:04 +0000

    [3] (MakeGuessUseCase) Define a test case - execute() makes a guess

--- tests/UseCase/MakeGuessUseCaseTest.php
+++ tests/UseCase/MakeGuessUseCaseTest.php
@@ -4,11 +4,63 @@ declare(strict_types=1);
 namespace SymfonyCon\Mastermind\UseCase;
 
 use PHPUnit\Framework\TestCase;
+use SymfonyCon\Mastermind\Game\Code;
+use SymfonyCon\Mastermind\Game\DecodingBoard;
+use SymfonyCon\Mastermind\Game\DecodingBoards;
+use SymfonyCon\Mastermind\Game\Feedback;
+use SymfonyCon\Mastermind\Game\GameUuid;
 
 class MakeGuessUseCaseTest extends TestCase
 {
-    public function test_it_is_initialized()
+    const GAME_UUID = '32915568-55ac-48d1-b817-9d8fa9cdff6b';
+
+    /**
+     * @var MakeGuessUseCase
+     */
+    private $useCase;
+    /**
+     * @var DecodingBoards|ObjectProphecy
+     */
+    private $boards;
+
+    /**
+     * @var DecodingBoard|ObjectProphecy
+     */
+    private $board;
+
+    /**
+     * @var Feedback|ObjectProphecy
+     */
+    private $feedback;
+
+    /**
+     * @var Code
+     */
+    private $code;
+
+    /**
+     * @var GameUuid
+     */
+    private $uuid;
+
+    protected function setUp()
     {
-        $this->assertInstanceOf(MakeGuessUseCase::class, new MakeGuessUseCase());
+        $this->board = $this->prophesize(DecodingBoard::class);
+        $this->code = $this->prophesize(Code::class)->reveal();
+        $this->uuid = GameUuid::existing(self::GAME_UUID);
+        $this->feedback = $this->prophesize(Feedback::class);
+
+        $this->boards = $this->prophesize(DecodingBoards::class);
+        $this->useCase = new MakeGuessUseCase($this->boards->reveal());
+    }
+
+    public function test_it_makes_a_guess()
+    {
+        $this->board->makeGuess($this->code)->willReturn($this->feedback);
+        $this->boards->get($this->uuid)->willReturn($this->board);
+
+        $this->useCase->execute($this->uuid, $this->code);
+
+        $this->board->makeGuess($this->code)->shouldHaveBeenCalled();
     }
 }
