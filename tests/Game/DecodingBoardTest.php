d7078be9049d34cf90892e2f52089a2d8a56e1f7
Jakub Zalas <jakub@zalas.pl>
2017-11-13 09:37:45 +0000

    [3] (DecodingBoard) Refactor the test case to improve readability

--- tests/Game/DecodingBoardTest.php
+++ tests/Game/DecodingBoardTest.php
@@ -9,20 +9,44 @@ class DecodingBoardTest extends TestCase
 {
     const NUMBER_OF_ATTEMPTS = 12;
 
+    /**
+     * @var DecodingBoard
+     */
+    private $board;
+
+    /**
+     * @var GameUuid
+     */
+    private $uuid;
+
+    /**
+     * @var Code|ObjectProphecy
+     */
+    private $guessCode;
+
+    /**
+     * @var Code|ObjectProphecy
+     */
+    private $secretCode;
+
+    protected function setUp()
+    {
+        $this->uuid = GameUuid::existing('547bf8e4-1a9c-492e-a0cf-165b809585a2');
+        $this->guessCode = $this->prophesize(Code::class);
+        $this->secretCode = $this->prophesize(Code::class);
+        $this->board = new DecodingBoard($this->uuid, $this->secretCode->reveal(), self::NUMBER_OF_ATTEMPTS);
+    }
+
     public function test_makeGuess_gives_feedback_on_the_guess()
     {
-        $uuid = GameUuid::existing('547bf8e4-1a9c-492e-a0cf-165b809585a2');
         $exactHits = 1;
         $colourHits = 3;
-        $guessCode = $this->prophesize(Code::class);
-        $secretCode = $this->prophesize(Code::class);
 
-        $secretCode->exactHits($guessCode)->willReturn($exactHits);
-        $secretCode->colourHits($guessCode)->willReturn($colourHits);
+        $this->secretCode->exactHits($this->guessCode)->willReturn($exactHits);
+        $this->secretCode->colourHits($this->guessCode)->willReturn($colourHits);
 
-        $board = new DecodingBoard($uuid, $secretCode->reveal(), self::NUMBER_OF_ATTEMPTS);
-        $feedback = $board->makeGuess($guessCode->reveal());
+        $feedback = $this->board->makeGuess($this->guessCode->reveal());
 
-        $this->assertEquals(new Feedback($guessCode->reveal(), $exactHits, $colourHits), $feedback);
+        $this->assertEquals(new Feedback($this->guessCode->reveal(), $exactHits, $colourHits), $feedback);
     }
 }
