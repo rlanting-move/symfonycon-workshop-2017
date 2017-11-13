d4f290a7798960d06f5bf567fa17635cb88cbda7
Jakub Zalas <jakub@zalas.pl>
2017-11-13 11:15:07 +0000

    [4] (RandomCodeMaker) Make the test pass

--- src/Game/RandomCodeMaker.php
+++ src/Game/RandomCodeMaker.php
@@ -7,5 +7,11 @@ final class RandomCodeMaker implements CodeMaker
 {
     public function newCode(int $length): Code
     {
+        $colours = array_intersect_key(
+            CodePeg::AVAILABLE_COLOURS,
+            array_flip(array_rand(CodePeg::AVAILABLE_COLOURS, $length))
+        );
+
+        return Code::fromString(implode(' ', $colours));
     }
 }
