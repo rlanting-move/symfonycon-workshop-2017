3a60ba812438612747ec9c7b8109c05f9a246ecf
Jakub Zalas <jakub@zalas.pl>
2017-11-13 10:18:21 +0000

    [3] (MakeGuessUseCase) Make the test pass

--- src/UseCase/MakeGuessUseCase.php
+++ src/UseCase/MakeGuessUseCase.php
@@ -3,9 +3,25 @@ declare(strict_types=1);
 
 namespace SymfonyCon\Mastermind\UseCase;
 
+use SymfonyCon\Mastermind\Game\Code;
+use SymfonyCon\Mastermind\Game\DecodingBoards;
+use SymfonyCon\Mastermind\Game\GameUuid;
+
 class MakeGuessUseCase
 {
-    public function execute($gameUuid, $code)
+    /**
+     * @var DecodingBoards
+     */
+    private $boards;
+
+    public function __construct(DecodingBoards $boards)
     {
+        $this->boards = $boards;
+    }
+
+    public function execute(GameUuid $gameUuid, Code $code)
+    {
+        $board = $this->boards->get($gameUuid);
+        $board->makeGuess($code);
     }
 }
