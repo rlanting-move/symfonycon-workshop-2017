fcb6ece253f5b300fe4bfb80855352d6122bb186
Jakub Zalas <jakub@zalas.pl>
2017-11-13 14:30:02 +0000

    Configure doctrine and install symfony console

--- src/Adapters/Web/Resources/config/bundles.php
+++ src/Adapters/Web/Resources/config/bundles.php
@@ -5,4 +5,6 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
 ];
