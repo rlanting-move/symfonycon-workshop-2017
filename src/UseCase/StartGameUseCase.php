6e95a03b467de5e6b5c0c580943ceb3b9d5fd451
Jakub Zalas <jakub@zalas.pl>
2017-11-13 10:02:38 +0000

    [3] (StartGameUseCase) Make the test pass

--- src/UseCase/StartGameUseCase.php
+++ src/UseCase/StartGameUseCase.php
@@ -3,9 +3,43 @@ declare(strict_types=1);
 
 namespace SymfonyCon\Mastermind\UseCase;
 
+use SymfonyCon\Mastermind\Game\CodeMaker;
+use SymfonyCon\Mastermind\Game\DecodingBoard;
+use SymfonyCon\Mastermind\Game\DecodingBoards;
+use SymfonyCon\Mastermind\Game\GameUuid;
+
 class StartGameUseCase
 {
+    /**
+     * @var DecodingBoards
+     */
+    private $boards;
+
+    /**
+     * @var CodeMaker
+     */
+    private $codeMaker;
+
+    /**
+     * @var int
+     */
+    private $numberOfAttempts;
+
+    public function __construct(DecodingBoards $boards, CodeMaker $codeMaker, int $numberOfAttempts)
+    {
+
+        $this->boards = $boards;
+        $this->codeMaker = $codeMaker;
+        $this->numberOfAttempts = $numberOfAttempts;
+    }
     public function execute($codeLength)
     {
+        $board = new DecodingBoard(
+            GameUuid::generated(),
+            $this->codeMaker->newCode($codeLength),
+            $this->numberOfAttempts
+        );
+
+        $this->boards->put($board);
     }
 }
